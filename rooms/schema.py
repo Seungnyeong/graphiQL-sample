546787998e5b81b16e765c0d2acad52ae9a91e7c
mellow0410@naver.com <mellow0410@gmail.com>
2020-03-01 00:46:47 +0900

    frist commit

---
+++ rooms/schema.py
@@ -0,0 +1,31 @@
+import graphene
+from graphene_django import DjangoObjectType
+from .models import Room
+
+
+class RoomType(DjangoObjectType):
+
+    user = graphene.Field("users.schema.UserType")
+
+    class Meta:
+        model = Room
+
+
+
+class RoomListResponse(graphene.ObjectType):
+    list = graphene.List(RoomType)
+    total = graphene.Int()
+
+class Query(object):
+
+    rooms = graphene.Field(RoomListResponse, page=graphene.Int())
+
+    def resolve_rooms(self, info, page=1):
+        page_size = 5
+        skipping = page_size * (page-1)
+        taking = page_size * page
+        rooms = Room.objects.all()[skipping:taking]   
+        total = Room.obejcts.count()
+        return RoomListResponse(arr=rooms, total=total)
+
+    
